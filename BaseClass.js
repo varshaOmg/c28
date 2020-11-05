f48fbab7afafe7dd06693316029d22dad351035c
varsha <rajasekaran.g@gmail.com>
2020-11-05 20:04:23 +0530

    changes in restitution

--- BaseClass.js
+++ BaseClass.js
@@ -1,9 +1,9 @@
 class BaseClass{
     constructor(x, y, width, height, angle) {
         var options = {
-            'restitution':0.8,
+            'restitution':0.3,
             'friction':1.0,
-            'density':1.0
+            'density':1.5
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
